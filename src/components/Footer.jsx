64aa4c2c5dcc0ecaab3a4089e8061b3a509c417c
Istan <thiago.sh26@gmail.com>
2024-01-21 17:19:43 -0300

    Terminando o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,10 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h1>Footer</h1>
+      <p>
+        Desenvolvido por Thiago Henrique durante o curso Origamid, todos
+        direitos pertencem a seu dono
+      </p>
     </footer>
   );
 };
